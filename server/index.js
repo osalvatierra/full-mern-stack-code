612907e1449a324b9fa40868b817c9b63c066093
osalvatierra <oscar@owebdesigns.com>
2023-12-29 13:26:08 -0500

    modifying if statement to check user

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.post("/api/login", async (req, res) => {
       user.password
     );
 
-    if (!user.email || isPasswordValid) {
+    if (!user || isPasswordValid) {
       return { status: "error", error: "Invalid Login" };
     }
 
