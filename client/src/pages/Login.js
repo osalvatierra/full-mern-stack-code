c70a2850cade215762b67e11165d7573e8b326b9
osalvatierra <oscar@owebdesigns.com>
2024-01-18 13:04:30 -0500

    CORS Finally working, now user pass issue

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,30 +5,56 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function loginUser(event) {
-    event.preventDefault();
-    fetch("https://full-mern-stack-server.onrender.com/api/login", {
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        // Retrieve the JWT token from the cookie
-        const jwtToken = Cookies.get("xaccesstoken");
-        console.log("JWT Token:", jwtToken);
-        if (data.success) {
-          alert("Login Successful");
-          window.location.href = "/dashboard";
-        } else {
-          alert("Please check your username and password ");
-        }
-      })
-      .catch((error) => console.error("Error:", error));
+  // async function loginUser(event) {
+  //   event.preventDefault();
+  //   fetch("https://full-mern-stack-server.onrender.com/api/login", {
+  //     method: "POST",
+  //     mode: "cors",
+  //     credentials: "include",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       // Retrieve the JWT token from the cookie
+  //       const jwtToken = Cookies.get("xaccesstoken");
+  //       console.log("JWT Token:", jwtToken);
+  //       if (data.success) {
+  //         alert("Login Successful");
+  //         window.location.href = "/dashboard";
+  //       } else {
+  //         alert("Please check your username and password ");
+  //       }
+  //     })
+  //     .catch((error) => console.error("Error:", error));
+
+    const response = await fetch(
+      "https://full-mern-stack-server.onrender.com/api/login",
+      {
+        method: "POST",
+        mode: "cors",
+        credentials: "include", // Include credentials (cookies)
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+
+    if (data.success) {
+      alert("Login Successful");
+      window.location.href = "/dashboard";
+    } else {
+      alert("Please check your username and password ");
+    }
   }
   return (
     <div>
