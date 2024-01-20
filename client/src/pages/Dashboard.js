63698e85d18acd4d81412302a41457753133e137
osalvatierra <oscar@owebdesigns.com>
2024-01-20 14:06:31 -0500

    logout error, fix v8

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -32,7 +32,7 @@ const Dashboard = () => {
 
   useEffect(() => {
     populateQuote();
-  }, []);
+  });
 
   //So you have to also include a GET request for /dashboard here and also add in index.js/routes so that you can
   //Access the cookies via fetch req, res and then routes will take care of the sending on the node server file!!!
