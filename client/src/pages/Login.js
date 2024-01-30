ff8d9f3fa7ce4acb2ad4f6dcb50d5b55f5b48c0f
osalvatierra <oscar@owebdesigns.com>
2024-01-30 14:26:24 -0500

    testing protected Route new code v3

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 function App() {
