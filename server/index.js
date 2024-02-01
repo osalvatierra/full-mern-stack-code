44a0ec130124f783769b12a77ae7dd5ef3b1dd01
osalvatierra <oscar@owebdesigns.com>
2024-02-01 13:53:24 -0500

    adding no-cache to see if pss works v2

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Cache-Control", "private");
   next();
 });
 
