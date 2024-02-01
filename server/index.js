13861c9683a7d04a430bd3bfa0691d0287a3efcb
osalvatierra <oscar@owebdesigns.com>
2024-02-01 14:42:43 -0500

    trblshooting cache issue v1

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "private");
+  res.setHeader("Cache-Control", "public");
   next();
 });
 
