267f45d45e3291588c366d686384da4bead1be72
osalvatierra <oscar@owebdesigns.com>
2024-02-03 17:45:30 -0500

    logout dashboard redirect troubleshooting, redirect issue causing loop, v15

--- server/index.js
+++ server/index.js
@@ -34,6 +34,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://full-mern-stack-code.onrender.com"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 let inOtherRoute = false;
 
 app.post("/api/register", async (req, res) => {
