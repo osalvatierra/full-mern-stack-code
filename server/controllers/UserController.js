baf1cc421d65ee7daf0317c804d060d295795240
osalvatierra <oscar@owebdesigns.com>
2024-02-15 14:51:43 -0500

    playing around w/ morgan to test routes

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,8 +1,12 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
 
 let inOtherRoute = false;
 
