ea2ede49cc882e5becd6a922823df72b01ed771f
osalvatierra <oscar@owebdesigns.com>
2024-02-16 14:58:13 -0500

    trblshooting, v1

--- server/index.js
+++ server/index.js
@@ -45,8 +45,8 @@ db.once("open", function () {
 // Routes
 const authRoutes = require("./routes/authRoutes");
 const quoteRoutes = require("./routes/quoteRoutes");
-app.use("/api/login", authRoutes);
-app.use("/api/quote", quoteRoutes);
+app.use("api/login", authRoutes);
+app.use("api/quote", quoteRoutes);
 
 // Error handling middleware
 // app.use((err, req, res) => {
