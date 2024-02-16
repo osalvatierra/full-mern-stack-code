cf755869358af99ebea389ee2951ca3837a697dd
osalvatierra <oscar@owebdesigns.com>
2024-02-16 15:21:47 -0500

    trblshooting, v5

--- server/index.js
+++ server/index.js
@@ -41,9 +41,8 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.setHeader("Cache-Control", "no-cache");
-  next();
 });
 
 // Routes
