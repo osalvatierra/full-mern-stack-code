77df10eb79fe3c2fe47c1035a5abf3dfdcf28939
osalvatierra <oscar@owebdesigns.com>
2024-03-15 13:26:42 -0400

    adding next in manual CORS entry, v6

--- server/index.js
+++ server/index.js
@@ -36,7 +36,10 @@ app.use((req, res, next) => {
     "GET, OPTIONS, HEAD, POST, PUT"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   if ("OPTIONS" == req.method) {
     res.sendStatus(200);
   } else {
