5dfa846beaf55338bcb80199c35dba3f6733dfde
osalvatierra <oscar@owebdesigns.com>
2024-03-21 13:58:14 -0400

    testing direct db w/ mongoose.connect vs dotevn ref, v1

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
@@ -12,40 +11,31 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(cookieParser());
 
+mongoose.connect(
+  `mongodb+srv://osalvatierra:YhG23YHt6WskEU6@cluster0.9edkxra.mongodb.net/login?retryWrites=true&w=majority`
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 const corsOptions = {
   origin: "https://full-mern-stack-code.onrender.com",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Origin, Content-Type, X-Auth-Token",
   optionsSuccessStatus: 204,
-  sameSite: "None",
+  sameSite: "none",
 };
-
 app.use(cors(corsOptions));
 
 app.use((req, res) => {
   res.setHeader("Cache-Control", "no-cache");
 });
 
-// Load environment variables from config.env file
-dotenv.config({ path: "./config.env" });
-
-// Database connection
-mongoose.connect(process.env.DB_CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
-
 // Routes
 const authRoutes = require("./routes/authRoutes");
 const quoteRoutes = require("./routes/quoteRoutes");
-
 app.use("/api/login", authRoutes);
 app.use("/api/quote", quoteRoutes);
 
