d4c0edf6e5ce1649d6b0499a64c18d25c43864a7
osalvatierra <oscar@owebdesigns.com>
2024-03-25 16:22:03 -0400

    experimenting w/ data.body output in AuthContext, v1

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -51,7 +51,7 @@ function AuthProvider({ children }) {
       if (data.success) {
         console.log(isAuthenticated);
         alert("Login Successful");
-        dispatch({ type: "Login", payload: data.user });
+        dispatch({ type: "Login", payload: data.body });
       } else {
         alert("Please check your username and password ");
       }
